f0ae9074233b7826e6165069a128250485300865
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-04-16 10:30:51 +0200

    [ALIEN-789] now supports a secured Cloudify manager

--- alien4cloud-cloudify-events/src/main/java/alien4cloud/paas/cloudify2/events/polling/RestClientManager.java
+++ alien4cloud-cloudify-events/src/main/java/alien4cloud/paas/cloudify2/events/polling/RestClientManager.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.cloudifysource.restclient.RestClient;
 import org.cloudifysource.restclient.exceptions.RestClientException;
 import org.springframework.stereotype.Component;
@@ -21,16 +24,37 @@ public class RestClientManager {
 
     private static final Logger log = Logger.getLogger(RestClientManager.class.getName());
 
+    private static final String CDFY_USERNAME = "cdfy.username";
+    private static final String CDFY_PASSWORD = "cdfy.password";
+
     private String version = "2.7.1";
     private String username;
     private String password;
+    private String url = "http://localhost:8100";
+    private boolean isManagerSecrured = false;
     private RestClient restClient;
+
+    @PostConstruct
+    private void PostConstruct() {
+        String springProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+        String[] profiles = null;
+        if (springProfile != null && (profiles = springProfile.split(",")) != null) {
+            isManagerSecrured = ArrayUtils.contains(profiles, "secure");
+        }
+
+        if (isManagerSecrured) {
+            log.info("Manager is secured");
+            username = System.getProperty(CDFY_USERNAME);
+            password = System.getProperty(CDFY_PASSWORD);
+            url = "https://localhost:8100";
+        }
+    }
 
     @SneakyThrows(IOException.class)
     public RestClient getRestClient() throws RestEventException {
         if (restClient == null) {
             try {
-                restClient = new RestClient(new URL("http://localhost:8100"), username, password, version);
+                restClient = new RestClient(new URL(url), username, password, version);
             } catch (RestClientException e) {
                 String msg = "Fail to create a Cloudify Rest Client. \n" + e.getMessageFormattedText();
                 log.severe(msg);
